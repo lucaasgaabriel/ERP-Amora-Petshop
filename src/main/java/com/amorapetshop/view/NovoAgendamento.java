84ba1cae9e3266e98c24e160e9d58ee4d9ea9b52
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-11-17 02:35:34 -0300

    frames_4

---
+++ src/main/java/com/amorapetshop/view/NovoAgendamento.java
@@ -0,0 +1,12 @@
+package com.amorapetshop.view;
+
+import javax.swing.*;
+
+public class NovoAgendamento {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton cancelarButton;
+    private JButton cadastarButton;
+}
---
+++ src/main/java/com/amorapetshop/view/NovoCliente.java
@@ -0,0 +1,16 @@
+package com.amorapetshop.view;
+
+import javax.swing.*;
+
+public class NovoCliente {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton cancelarButton;
+    private JButton cadastarButton;
+    private JComboBox comboBox1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
