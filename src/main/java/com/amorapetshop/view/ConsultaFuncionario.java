f00fcabd69712bf1bb71d18e54b3c296572225ee
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-11-17 00:33:11 -0300

    frames

---
+++ src/main/java/com/amorapetshop/view/ConsultaFuncionario.java
@@ -0,0 +1,13 @@
+package com.amorapetshop.view;
+
+import javax.swing.*;
+
+public class ConsultaFuncionario {
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
---
+++ src/main/java/com/amorapetshop/view/index.java
@@ -0,0 +1,17 @@
+package com.amorapetshop.view;
+
+import javax.swing.*;
+
+public class index {
+    private JPanel panel1;
+    private JButton funcionarioButton;
+    private JButton produtoButton;
+    private JButton clienteButton;
+    private JButton animalButton;
+    private JButton servicosButton;
+    private JPanel panel2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
