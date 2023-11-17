ecc6d709a21817ffc20f46f7272c48419d062db2
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-11-17 01:28:43 -0300

    frames_3

---
+++ src/main/java/com/amorapetshop/view/ConsultarCliente.java
@@ -0,0 +1,13 @@
+package com.amorapetshop.view;
+
+import javax.swing.*;
+
+public class ConsultarCliente {
+    private JTable table1;
+    private JPanel panel1;
+    private JButton voltarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton cadastrarButton;
+    private JButton pesquisarButton;
+}
---
+++ src/main/java/com/amorapetshop/view/ConsultarProduto.java
@@ -0,0 +1,13 @@
+package com.amorapetshop.view;
+
+import javax.swing.*;
+
+public class ConsultarProduto {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton cadastrarButton;
+    private JButton pesquisarButton;
+    private JButton voltarButton;
+    private JTable table1;
+    private JButton button1;
+}
