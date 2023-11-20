041fb34eb5c58c9a6c687ebf0e65a49de7b6e6b2
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-11-20 16:42:36 -0300

    Cadastrar_aniamais

--- src/main/java/com/amorapetshop/view/ConsultaAnimal.java
+++ src/main/java/com/amorapetshop/view/ConsultaAnimal.java
@@ -3,6 +3,7 @@ package com.amorapetshop.view;
 import com.amorapetshop.model.Animal;
 import com.amorapetshop.model.dao.AnimalJpaDao;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import java.awt.event.*;
 
 public class ConsultaAnimal {
     private JTable tabela_animais;
+    private DefaultTableModel modelo;
     private JTextField nome_input_animal;
     private JTextField Dono_input_animal;
     private JTextField especie_input_animal;
@@ -25,47 +27,14 @@ public class ConsultaAnimal {
     private JButton editarButton;
     private JButton excluirButton;
     private JPanel animais_consulta;
+    private JPanel tablepainel;
 
     private List<Animal> listaDeAnimais;
+    private Animal animalSelecionado;
 
 
     public ConsultaAnimal() {
-        listaDeAnimais = new ArrayList<>(); // Você precisará importar java.util.ArrayList
 
-
-        nome_input_animal.addInputMethodListener(new InputMethodListener() {
-            @Override
-            public void inputMethodTextChanged(InputMethodEvent event) {
-
-            }
-
-            @Override
-            public void caretPositionChanged(InputMethodEvent event) {
-
-            }
-        });
-        Dono_input_animal.addInputMethodListener(new InputMethodListener() {
-            @Override
-            public void inputMethodTextChanged(InputMethodEvent event) {
-
-            }
-
-            @Override
-            public void caretPositionChanged(InputMethodEvent event) {
-
-            }
-        });
-        especie_input_animal.addInputMethodListener(new InputMethodListener() {
-            @Override
-            public void inputMethodTextChanged(InputMethodEvent event) {
-
-            }
-
-            @Override
-            public void caretPositionChanged(InputMethodEvent event) {
-
-            }
-        });
         cadastrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -105,16 +74,16 @@ public class ConsultaAnimal {
                         List<Animal> resultados = animalDao.buscaFiltro(animalConsulta);
 
                         // Atualiza o modelo da tabela com os resultados
-                        DefaultTableModel modeloTabela = (DefaultTableModel) tabela_animais.getModel();
+                        //DefaultTableModel modeloTabela = (DefaultTableModel) tabela_animais.getModel();
 
                         // Limpa o modelo antes de adicionar os novos resultados
-                        modeloTabela.setRowCount(0);
+                        //modeloTabela.setRowCount(0);
 
                         // Adiciona os resultados ao modelo da tabela
-                        for (Animal animal : resultados) {
-                            Object[] rowData = {animal.getId(), animal.getNome(), animal.getEspecie(), animal.getRaca()};
-                            modeloTabela.addRow(rowData);
-                        }
+                        //for (Animal animal : resultados) {
+                       //     Object[] rowData = {animal.getId(), animal.getNome(), animal.getEspecie(), animal.getRaca()};
+                        //    modeloTabela.addRow(rowData);
+                       // }
                     }
                 });
 
@@ -149,12 +118,13 @@ public class ConsultaAnimal {
                 currentFrame.repaint();
             }
         });
-        tabela_animais.addComponentListener(new ComponentAdapter() {
+
+        /*tabela_animais.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
             }
-        });
+        });*/
 
         animais_consulta.addComponentListener(new ComponentAdapter() {
             @Override
@@ -163,23 +133,68 @@ public class ConsultaAnimal {
             }
         });
 
+        tablepainel.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
 
+                // Obtém as dimensões atualizadas do painel
+                int larguraPainel = animais_consulta.getWidth();
+                int alturaPainel = animais_consulta.getHeight();
+
+                tabela_animais.setBounds(10, 10, larguraPainel - 20, alturaPainel - 20);
+            }
+        });
+
+
+        tablepainel = new JPanel();
+
+        // Configurações da Tabela que irá listar as pessoas
+        tablepainel.setLayout(null);
+        tabela_animais = new JTable();
+
+        // Definindo as colunas da tabela
+        String[] colunas = {"Nome", "Espécie", "Raça"};
+        String[][] objetos = {{"", "", ""}};
+
+        DefaultTableModel modeloTabela = new DefaultTableModel(objetos, colunas) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        tabela_animais.setModel(modeloTabela);
+
+        JScrollPane scroll = new JScrollPane(tabela_animais);
+        scroll.setSize(664, 212);
+        scroll.setLocation(10, 177);
+        tablepainel.add(scroll);
+
+        animais_consulta.add(tablepainel);
     }
 
     public JPanel getAnimaisConsulta() {
         return animais_consulta;
     }
 
+
     public void carregarTodosDados() {
-        AnimalJpaDao animalDao = new AnimalJpaDao();
-        List<Animal> todosAnimais = animalDao.buscaTodos();
+        String[] colunas = {"Nome", "CPF","e-mail"};
 
-        DefaultTableModel modeloTabela = (DefaultTableModel) tabela_animais.getModel();
-        modeloTabela.setRowCount(0);
-
-        for (Animal animal : todosAnimais) {
-            Object[] rowData = {animal.getId(), animal.getNome(), animal.getEspecie(), animal.getRaca()};
-            modeloTabela.addRow(rowData);
+        String[][] objetos = new String [listaDeAnimais.size()][3];
+        int i=0;
+        for(Animal animal:listaDeAnimais) {
+            objetos[i]= new String[]{animal.getNome(),animal.getRaca(),animal.getEspecie()};
+            i++;
         }
+
+        modelo = (new DefaultTableModel(objetos,colunas){
+            @Override
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        });
+
+        tabela_animais.setModel(modelo);
     }
 }
