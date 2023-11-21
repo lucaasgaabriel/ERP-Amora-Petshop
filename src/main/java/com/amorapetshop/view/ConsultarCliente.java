ef7c96a816e3750cd19dbe3d9df2b7502111e455
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2023-11-21 18:52:35 -0300

    tabela Cliente

--- src/main/java/com/amorapetshop/view/ConsultarCliente.java
+++ src/main/java/com/amorapetshop/view/ConsultarCliente.java
@@ -1,6 +1,8 @@
 package com.amorapetshop.view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,28 +10,28 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 public class ConsultarCliente {
-    private JTable table1;
+    private JTable tabelaCliente;
     private JPanel ClienteConsultapainel;
     private JButton voltarButton;
     private JTextField NomeClinete;
     private JTextField CPFCliente;
-    private JButton cadastrarButton;
+    private JButton NovoButton;
     private JButton pesquisarButton;
+    private JButton excluirButton;
+    private JButton Editar;
+    private JPanel cabeçalhoCleinte;
+    private JPanel conteudoCliente;
+    private DefaultTableModel tableModel;
+    private JPanel RodapeCliente;
 
     public ConsultarCliente() {
-        ClienteConsultapainel.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                super.componentResized(e);
-            }
-        });
         pesquisarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
-        cadastrarButton.addActionListener(new ActionListener() {
+        NovoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Obtém a janela atual associada ao botão clicado
@@ -45,12 +47,6 @@ public class ConsultarCliente {
                 // Atualiza a exibição
                 currentFrame.revalidate();
                 currentFrame.repaint();
-            }
-        });
-        table1.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                super.componentResized(e);
             }
         });
         voltarButton.addActionListener(new ActionListener() {
@@ -70,6 +66,49 @@ public class ConsultarCliente {
                 currentFrame.repaint();
             }
         });
+        excluirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        Editar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        tabelaCliente.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+            }
+        });
+        String[] colunas = {"id","Nome", "CPF"};
+        String[][] objetos = {{"", "", ""}};
+
+        tableModel = new DefaultTableModel(objetos, colunas) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        conteudoCliente.setLayout(new BorderLayout());
+        conteudoCliente.add(new JScrollPane(tabelaCliente), BorderLayout.CENTER);
+
+        // Defina o modelo da tabela
+        tabelaCliente.setModel(tableModel);
+
+        TableColumnModel columnModel = tabelaCliente.getColumnModel();
+        columnModel.getColumn(0).setMinWidth(0);
+        columnModel.getColumn(0).setMaxWidth(0);
+        columnModel.getColumn(0).setWidth(0);
+
+        tabelaCliente.setAutoCreateRowSorter(true);
+        tabelaCliente.getTableHeader().setReorderingAllowed(false);
+        tabelaCliente.getTableHeader().setVisible(true);
+        //TabelaFuncionarios.setVisible(true);
         ClienteConsultapainel.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
