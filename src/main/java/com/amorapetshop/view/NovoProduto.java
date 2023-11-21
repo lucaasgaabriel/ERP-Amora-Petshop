8a803267f58d39c4c4118cea17d6185ebdb590b2
Arthur <arthurmarcal639@gmail.com>
2023-11-21 00:25:56 -0300

    foi iniciada a integração para cadastrar produtos

--- src/main/java/com/amorapetshop/view/NovoProduto.java
+++ src/main/java/com/amorapetshop/view/NovoProduto.java
@@ -1,11 +1,17 @@
 package com.amorapetshop.view;
 
+import com.amorapetshop.model.Produto;
+import com.amorapetshop.controller.ProdutoController;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date
 
 public class NovoProduto {
     private JTextField nomeProduto;
@@ -18,9 +24,41 @@ public class NovoProduto {
 
 
     public NovoProduto() {
+        ProdutoController produtoController = new ProdutoController();
+
         cadastarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String formatoData = "dd-MM-aaaa";
+                SimpleDateFormat format = new SimpleDateFormat(formatoData);
+                // Obtenha os dados do animal dos campos de texto
+                String nome = nomeProduto.getText();
+                String dataString = DT_Entrada.getText();
+                String valor = Valor.getText();
+
+                try {
+                    Date data = format.parse(dataString);
+                    // Crie uma instância da entidade Animal com os dados
+                    Produto novoProduto = new Produto();
+                    novoProduto.setNome(nome);
+                    novoProduto.setDataEntrada(data);
+                    novoProduto.setValor(valor);
+
+                    produtoController.salvar(novoProduto);
+
+                } catch (ParseException x) {
+                    x.printStackTrace();
+
+
+
+
+
+
+
+                // Limpe os campos de texto ou faça outras ações após o cadastro
+                nomeProduto.setText("");
+                DT_Entrada.setText("");
+                Valor.setText("");
 
             }
         });
