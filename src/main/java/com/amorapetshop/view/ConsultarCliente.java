7fecec2c221f6321124f0afe75c83c60d0b625a7
Arthur <arthurmarcal639@gmail.com>
2023-11-22 00:05:06 -0300

    feito as funções dos botões excluir, editar e pesquisar da pagina ConsultarCliente.java

--- src/main/java/com/amorapetshop/view/ConsultarCliente.java
+++ src/main/java/com/amorapetshop/view/ConsultarCliente.java
@@ -1,5 +1,10 @@
 package com.amorapetshop.view;
 
+
+import com.amorapetshop.controller.ClienteController;
+import com.amorapetshop.model.Animal;
+import com.amorapetshop.model.Cliente;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
@@ -8,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.List;
 
 public class ConsultarCliente {
     private JTable tabelaCliente;
@@ -23,13 +29,34 @@ public class ConsultarCliente {
     private JPanel conteudoCliente;
     private DefaultTableModel tableModel;
     private JPanel RodapeCliente;
+    private List<Cliente> cliente;
+    private ClienteController clienteController;
 
     public ConsultarCliente() {
         pesquisarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Obtém os critérios de pesquisa do formulário
+                String nome = NomeClinete.getText();
+                String cpf = CPFCliente.getText();
+
+                // Cria uma instância de cliente com os critérios de pesquisa
+                Cliente clienteConsulta = new Cliente();
+                clienteConsulta.setNome(nome);
+                clienteConsulta.setCpf(cpf);
+
+                // Chama o método buscarComFiltro no back-end
+                List<Cliente> resultados = clienteController.buscarFiltro(clienteConsulta);
+
+                // Atualiza a tabela com os resultados da pesquisa
+                atualizarTabela(resultados);
+
+                NomeClinete.setText("");
+                CPFCliente.setText("");
 
             }
+
+
         });
         NovoButton.addActionListener(new ActionListener() {
             @Override
@@ -69,6 +96,47 @@ public class ConsultarCliente {
         excluirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Obtém a linha selecionada na tabela
+                int selectedRow = tabelaCliente.getSelectedRow();
+
+                // Verifica se uma linha foi realmente selecionada
+                if (selectedRow >= 0) {
+                    // Obtém os dados da linha selecionada
+                    long id = (long) tabelaCliente.getValueAt(selectedRow, 0);
+                    String nome = (String) tabelaCliente.getValueAt(selectedRow, 1);
+                    String cpf = (String) tabelaCliente.getValueAt(selectedRow, 2);
+                    String telefone = (String) tabelaCliente.getValueAt(selectedRow, 3);
+                    String sexo = (String) tabelaCliente.getValueAt(selectedRow, 4);
+
+                    // Cria uma instância de Animal com os dados da linha selecionada
+                    Cliente clienteParaExcluir = new Cliente();
+                    clienteParaExcluir.setId(id);
+                    clienteParaExcluir.setNome(nome);
+                    clienteParaExcluir.setCpf(cpf);
+                    clienteParaExcluir.setTelefone(telefone);
+                    clienteParaExcluir.setSexo(sexo);
+
+                    // Confirmação de exclusão
+                    int option = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja excluir este cliente?", "Confirmação de exclusão", JOptionPane.YES_NO_OPTION);
+
+                    if (option == JOptionPane.YES_OPTION) {
+                        // Realiza a exclusão do animal no banco de dados
+                        clienteController.excluir(clienteParaExcluir);
+
+                        // Chama o método buscarComFiltro no back-end
+                        List<Cliente> resultados = clienteController.buscarFiltro(clienteParaExcluir);
+
+                        // Atualiza a tabela com os resultados da pesquisa
+                        atualizarTabela(resultados);
+
+                        // Atualiza a tabela após a exclusão
+                        carregarDadosNaTabela();
+                    }
+                } else {
+                    // Se nenhuma linha estiver selecionada, exiba uma mensagem de erro
+                    JOptionPane.showMessageDialog(null, "Por favor, selecione uma linha para excluir.",
+                            "Nenhuma Linha Selecionada", JOptionPane.ERROR_MESSAGE);
+                }
 
             }
         });
@@ -115,6 +183,29 @@ public class ConsultarCliente {
                 super.componentResized(e);
             }
         });
+    }
+    private void carregarDadosNaTabela() {
+        // Limpa os dados existentes na tabela
+        tableModel.setRowCount(0);
+
+        // Obtém a lista de cliente do banco de dados
+        cliente = clienteController.buscarTodos();
+
+        // Preenche a tabela com os dados obtidos
+        for (Cliente cliente : cliente) {
+            tableModel.addRow(new Object[]{cliente.getId(), cliente.getNome(), cliente.getCpf(), cliente.getTelefone(),cliente.getSexo()});
+            // Adicione mais colunas conforme necessário
+        }
+    }
+    private void atualizarTabela(List<Cliente> resultados) {
+        // Limpa os dados existentes na tabela
+        tableModel.setRowCount(0);
+
+        // Preenche a tabela com os dados obtidos
+        for (Cliente cliente : resultados) {
+            tableModel.addRow(new Object[]{cliente.getId(), cliente.getNome(), cliente.getCpf(), cliente.getTelefone(),cliente.getSexo()});
+            // Adicione mais colunas conforme necessário
+        }
     }
     public JPanel getConsultaClientepainel() {
         return ClienteConsultapainel;
